4c12a48dd5aebd1a82809592cdb0ae067bd7788b
pokhengly <henglypok21012021@gmail.com>
2025-01-18 16:35:29 +0700

    Initial Commit

---
+++ InteractiveTaPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InteractiveTaPractice
+//
+//  Created by KAK-LY on 18/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InteractiveTaPractice/InteractiveTaPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveTaPracticeApp.swift
+//  InteractiveTaPractice
+//
+//  Created by KAK-LY on 18/1/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveTaPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
